ae2cf5a05ee36b717999abb2a664978869c90d74
Purnesh <180254849+Purnesh-codes@users.noreply.github.com>
2026-08-27 12:29:42 +0530

    added logger middleware

---
+++ middlewares/logger.js
@@ -0,0 +1,6 @@
+export const logger = (req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+  );
+  next();
+};
